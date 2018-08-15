cab1115a54e65dd50ff7a9338042b5c3ffbafc7b
Manoj Modhwadia <manojmodhwadia@outlook.com>
2018-08-15 19:25:28 +0100

    working integration test

--- test/test-blogPostRouter.js
+++ test/test-blogPostRouter.js
@@ -3,7 +3,7 @@ const chaihttp = require('chai-http');
 
 const { app, runServer, closeServer } = require('../server');
 
-const expect = require('chai').expect;
+const expect = chai.expect;
 
 chai.use(chaihttp);
 
@@ -20,11 +20,11 @@ describe('Blogs', function() {
         .then(function(res) {
             expect(res).to.have.status(200);
             expect(res).to.be.json;
-            expect(res).to.be.a('array');
+            // expect(res).to.be.a('array');
             expect(res.body.length).to.be.above(0);
             res.body.forEach(function(blog) {
-                expect(item).to.be.a('object');
-                expect(item).to.have.all.keys('title', 'content', 'author');
+                expect(blog).to.be.a('object');
+                expect(blog).to.have.all.keys('title', 'content', 'author', 'id', 'publishDate');
             });
         });
     });
@@ -36,32 +36,31 @@ describe('Blogs', function() {
             .then(function(res) {
                 expect(res).to.have.status(201);
                 expect(res).to.be.json;
-                expect(res.body).to.include.keys('title', 'content', 'author', 'publishDate');
+                expect(res.body).to.have.all.keys('id', 'title', 'content', 'author', 'publishDate');
                 expect(res.body.id).to.not.equal(null);
-                expect(res.body).to.deep.equal(Object.assign(newBlog, {id: res.body.id}));
+                expect(res.body.title).to.equal(newBlog.title);
+                expect(res.body.content).to.equal(newBlog.content);
+                expect(res.body.author).to.equal(newBlog.author);
             });
     });
     it('should update a blog on PUT', function() {
-        const updateData = {
-            title: 'What is Lorem Ipsum?',
-            content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-            author: 'Shane McGinty'
-        };
         return chai.request(app)
             .get('/blog-posts')
             .then(function(res) {
+                const updateData = Object.assign(res.body[0], {
+                    title: 'What is Lorem Ipsum?',
+                    content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+                });
                 updateData.id = res.body[0].id;
                 return chai.request(app)
                     .put(`/blog-posts/${updateData.id}`)
                     .send(updateData)
             })
             .then(function(res) {
-                expect(res).to.have.status(200);
-                expect(res).to.be.json;
-                expect(res).to.be.a('object');
-                expect(res).to.deep.equal(updateData);
+                expect(res).to.have.status(204);
             });
     });
+
     it('should delete a blog on DELETE', function() {
         return chai.request(app)
         .get('/blog-posts')
