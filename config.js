2458941b0dbeec20f9c7e48c945b6dfd99e66dcf
Manoj Modhwadia <manojmodhwadia@outlook.com>
2018-08-24 14:43:23 +0100

    added heroku to config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 "use strict";
-exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost/blog-app";
+exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://muffinman:muffinman75@ds125402.mlab.com:25402/blog-data";
 exports.PORT = process.env.PORT || 8080;
